cb9bbfa822faff8f0510adc50146eb2b397e867b
dsouzd <117829012+dsouzd@users.noreply.github.com>
2023-04-08 19:21:54 +0530

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,16 +1,30 @@
-import { Observable } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 
-const observable$ = new Observable<string>((subscriber) => {
-  console.log('Observable executed');
-  subscriber.next('Alice');
-  setTimeout(() => subscriber.next('Ben'), 2000);
-  setTimeout(() => subscriber.next('Charlie'), 4000);
+const observable1$ = new Observable<any>((Subscriber) => {
+  Subscriber.next('Alice');
+  setTimeout(() => Subscriber.next('Ben'), 2000);
+  setTimeout(() => Subscriber.next('Charlie'), 4000);
+  setTimeout(() => Subscriber.next('Derren'), 6000);
+  setTimeout(() => Subscriber.next('Dsouza'), 8000);
 });
 
-console.log('Subscription 1 starts');
-observable$.subscribe((value) => console.log('Subscription 1:', value));
+// const observable$ = new Observable<string>((subscriber) => {
+//   console.log('Observable executed');
+//   subscriber.next('Alice');
+//   setTimeout(() => subscriber.next('Ben'), 2000);
+//   setTimeout(() => subscriber.next('Charlie'), 4000);
+// });
 
-setTimeout(() => {
-  console.log('Subscription 2 starts');
-  observable$.subscribe((value) => console.log('Subscription 2:', value));
-}, 1000);
+// console.log('Subscription 1 starts');
+// observable$.subscribe((value) => console.log('Subscription 1:', value));
+
+// setTimeout(() => {
+//   console.log('Subscription 2 starts');
+//   observable$.subscribe((value) => console.log('Subscription 2:', value));
+// }, 1000);
+
+//const reference = observable1$.subscribe((value) => console.log(value));
+
+// setTimeout(() => {
+//   reference.unsubscribe();
+// }, 5000);
